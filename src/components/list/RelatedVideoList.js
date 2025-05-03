3daa1e18c35d8f52610ef765a95a961228b164c3
Rayhan Kabir <rayhan.kabir29@gmail.com>
2025-05-03 15:56:16 +0600

    video list

---
+++ src/components/list/RelatedVideoList.js
@@ -0,0 +1,10 @@
+import RelatedVideoListItem from "./RelatedVideoListItem";
+
+const RelatedVideoList =()=> {
+    return (
+        <div class="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+            <RelatedVideoListItem />
+        </div>
+    );
+}
+export default RelatedVideoList;
