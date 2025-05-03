4277888646d14e6a1c6c6fa3f073a29c61e8cd41
Rayhan Kabir <rayhan.kabir29@gmail.com>
2025-05-03 16:33:00 +0600

    update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./Search";
+import logo from "../../assets/images/logo.png"
 
 const Navbar = () => {
   return (
@@ -7,13 +8,13 @@ const Navbar = () => {
       <nav className="bg-slate-100 shadow-md">
         <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
           <a href="/">
-            <img className="h-10" src="./assets/lws.svg" alt="Learn with Sumit" />
+            <img className="h-10"  src={logo} alt="Learn with Sumit" />
           </a>
           <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
             <Search />
             <img
               className="inline h-4 cursor-pointer"
-              src="./assets/search.svg"
+              src={logo}
               alt="Search"
             />
           </div>
