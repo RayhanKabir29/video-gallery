08a81fb668bf14a11e5d2a68b43c0969194f3bc4
Rayhan Kabir <rayhan.kabir29@gmail.com>
2025-05-03 15:52:07 +0600

    file stricture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+import Tags from '../components/tags/Tags';
+import Videos from '../components/videos/Videos';
+import Pagination from '../components/ui/Pagination';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home Page</h2>
+           <Navbar/>
+           <Tags/>
+           <Videos/>
+           <Pagination/>
+           <Footer/>
         </div>
     );
 };
