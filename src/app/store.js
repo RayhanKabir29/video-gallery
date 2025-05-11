1131e84c330ec18e25ec2751271771ffc14e47dc
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-05-11 15:34:42 +0600

    filter reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import videosReducer from '../features/videos/videosSlice';
 import tagsReducer from '../features/tags/tagsSlice';
 import videoReducer from '../features/video/videoSlice';
-import relatedVideoSlice from '../features/relatedVideos/relatedVideoSlice'
+import relatedVidoReducer from '../features/relatedVideos/relatedVideoSlice'
+import filterReducer from '../features/filter/filterSlice'
 
 export const store = configureStore({
   reducer: {
     videos : videosReducer,
     tags: tagsReducer,
     video: videoReducer,
-    relatedVideos: relatedVideoSlice,
+    relatedVideos: relatedVidoReducer,
+    filter:filterReducer,
   },
 });
